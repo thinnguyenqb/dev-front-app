a3c273e8b23979d74d5c1c0cf24154a857068339
EricNguyen99 <vanthin7111999@gmail.com>
2021-07-26 14:58:03 +0700

    2. Authentication

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.json({msg: "Hello"})
-})
+//Routes
+app.use('/api', require('./routes/authRouter'))
 
-//Connect DB
+
+//Connect DB    
 const URI = process.env.MONGODB_URL
+
 mongoose.connect(URI, {
     useCreateIndex: true,
     useFindAndModify: false,
