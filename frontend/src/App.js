3c9cc6c9acdb7877092a742dcdbe75abac01e2d0
EricNguyen99 <vanthin7111999@gmail.com>
2021-07-31 13:02:14 +0700

    protect routes and authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import PageRender from "./PageRender";
+import PageRender from "./customRouter/PageRender";
+import PrivateRouter from "./customRouter/PrivateRouter";
+
 import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
@@ -26,8 +28,10 @@ const App = () => {
         {auth.token && <Header/>}
         <div className="main">
           <Route exact path="/" component={auth.token ? Home : Login} />
-          <Route exact path="/:page" component={PageRender} />
-          <Route exact path="/:page/:id" component={PageRender} />
+          <Route exact path="/register" component={Register} />
+          
+          <PrivateRouter exact path="/:page" component={PageRender} />
+          <PrivateRouter exact path="/:page/:id" component={PageRender} />
         </div>
       </div>
     </Router>
---
+++ frontend/src/customRouter/PrivateRouter.js
@@ -0,0 +1,8 @@
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRouter = ({props}) => {
+  const firstlogin = localStorage.getItem('firstlogin')
+  return firstlogin ? <Route {...props} /> : <Redirect to="/" />
+}
+
+export default PrivateRouter
