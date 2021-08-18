cd852f6ef6b188d60709ee25297be2761943e4a8
EricNguyen99 <vanthin7111999@gmail.com>
2021-08-18 21:49:01 +0700

    add updateUser(BE)

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -13,6 +13,35 @@ const userCtrl = {
     }
   },
 
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.params.id).select('-password')
+      if(!user) res.status(400).json({msg: "User does not exist"})
+
+      res.json({user})
+  
+    }catch(err){
+      return res.status(500).json({msg: err.message})
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const { avatar, fullname, mobile, address, story, website, gender } = req.body
+      if (!fullname) res.status(400).json({ msg: "Please add your full name." })
+
+      await Users.findOneAndUpdate({ _id: req.user.id }, {
+        avatar, fullname, mobile, address, story, website, gender 
+      })
+
+      res.json({ msg: "Update Success!" })
+      
+    }catch(err){
+      return res.status(500).json({msg: err.message})
+    }
+  },
+
+
+
 }
 
 module.exports = userCtrl
