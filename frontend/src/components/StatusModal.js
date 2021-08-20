c2699965cfdd03e96e21d15aac1f488123d25831
EricNguyen99 <vanthin7111999@gmail.com>
2021-08-20 15:46:02 +0700

    update up image UI

--- frontend/src/components/StatusModal.js
+++ frontend/src/components/StatusModal.js
@@ -3,10 +3,29 @@ import { useSelector, useDispatch } from "react-redux";
 import { GLOBALTYPES } from "../redux/actions/globalTypes";
 
 const StatusModal = () => {
-  const { auth } = useSelector((state) => state);
+  const { auth, theme } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const [content, setContent] = useState("");
+  const [images, setImages] = useState([])
+
+  const handleChangeImages = e => {
+    const files = [...e.target.files]
+    let err = ""
+    let newImages = []
+
+    files.forEach(file => {
+      if (!file) return err = "File does not exist"
+      
+      if (file.type !== 'image/jpeg' && file.type !== 'image/png')
+        return err = "Image format is incorrect."
+      
+      return newImages.push(file)
+    })
+
+    if (err) dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err}})
+    setImages([...images, ...newImages])
+  }
 
   return (
     <div className="status_modal">
@@ -28,7 +47,23 @@ const StatusModal = () => {
             placeholder={`${auth.user.username}, what are you thinking?`}
             onChange={(e) => setContent(e.target.value)}
           />
-          <div className="input_images">
+          
+
+          <div className="show_images">
+            {
+              images.map((img, index) => (
+                <div key={index} id="file_img">
+                  <img src={URL.createObjectURL(img)}
+                    alt="images" className="img-thumbnail"
+                    style={{filter: theme ? 'invert(1)' : 'invert(0)'}}
+                  />
+                  <i className="far fa-trash-alt" id="image_trash"></i>
+                  {/* <span>&times;</span> */}
+                </div>
+              ))
+            }
+          </div>
+          <div className="input_images" style={{filter: theme ? 'invert(1)' : 'invert(0)'}}>
             <i className="fas fa-camera" />
             <div className="file_upload">
               <i className="fas fa-image" />
@@ -38,12 +73,13 @@ const StatusModal = () => {
                 id="file"
                 multiple
                 accept="image/*"
+                onChange={handleChangeImages}
               />
             </div>
           </div>
         </div>
         <div className="status_footer mt-3">
-          <button className="btn btn-more w-100">Post</button>
+          <button className="btn btn-more w-100" style={{filter: theme ? 'invert(1)' : 'invert(0)'}}>Post</button>
         </div>
       </form>
     </div>
