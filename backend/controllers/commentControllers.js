68555a0a334a8693d9a424aeff7baded35d8c27c
EricNguyen99 <vanthin7111999@gmail.com>
2021-08-27 21:42:25 +0700

    add handle delete replyComment & comment

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -4,10 +4,10 @@ const Posts = require('../models/postModel')
 const commentCtrl = {
   createComment: async (req, res) => {
     try {
-      const { postId, content, tag, reply } = req.body;
+      const { postId, content, tag, reply, postUserId } = req.body;
 
       const newComment = new Comments({
-        user: req.user._id, content, tag, reply
+        user: req.user._id, content, tag, reply, postUserId, postId
       })
 
       await Posts.findOneAndUpdate({ _id: postId }, {
@@ -78,6 +78,27 @@ const commentCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+
+  deleteComment: async (req, res) => {
+    try {
+      const comment = await Comments.findOneAndDelete({
+        _id: req.params.id,
+        $or: [
+          { user: req.user._id },
+          { postUserId: req.user._id}
+        ]
+      })
+      //console.log(comment)
+      await Posts.findOneAndUpdate({ _id: comment.postId }, {
+        $pull: {comments: req.params.id}
+      })
+      res.json({ msg: 'Delete Comment!' })
+      
+    } catch (err) {
+      return res.status(500).json({msg: err.message})
+    }
+  }
 }
+  
 
 module.exports = commentCtrl;
--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -7,6 +7,9 @@ router.post('/comment', auth, commentCtrl.createComment)
 router.patch('/comment/:id', auth, commentCtrl.updateComment)
 
 router.patch('/comment/:id/like', auth, commentCtrl.likeComment)
+
 router.patch('/comment/:id/unlike', auth, commentCtrl.unLikeComment)
+
+router.delete('/comment/:id', auth, commentCtrl.deleteComment)
 
 module.exports = router;
