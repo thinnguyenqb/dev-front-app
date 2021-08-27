9fec60dc52975117da536238bc4b9c72ff43cabc
EricNguyen99 <vanthin7111999@gmail.com>
2021-08-27 10:38:11 +0700

    update (show, hide) reply comments

--- frontend/src/components/home/comments/CommentDisplay.js
+++ frontend/src/components/home/comments/CommentDisplay.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CommentCard from './CommentCard'
 
-const CommentDisplay = ({comment, post, replyCm }) => {
+const CommentDisplay = ({ comment, post, replyCm }) => {
+  const [showRep, setShowRep] = useState([])
+  const [next, setNext] = useState(1)
+
+  useEffect(() => {
+    setShowRep(replyCm.slice(replyCm.length - next))
+  }, [replyCm, next])
+  
   return (
     <div className="comment_display">
       <CommentCard comment={comment} post={post} commentId={comment._id}>
         <div className="pl-5">
           {
-            replyCm.map((item, index) => (
+            showRep.map((item, index) => (
               item.reply &&
               <CommentCard key={index} comment={item} post={post} commentId={comment._id}
               />
             ))
+          }
+          {
+            replyCm.length - next > 0
+              ?
+              <div style={{ cursor: 'pointer', color: 'crimson' }}
+                onClick={() => setNext(next + 10)}
+              >
+                See more moments...
+              </div>
+              : replyCm.length > 1 &&
+              <div style={{ cursor: 'pointer', color: 'crimson' }}
+                onClick={() => setNext(2)}
+              >
+                Hige moments...
+              </div>
           }
         </div>
       </CommentCard>
