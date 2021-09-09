b3549d7ef139661c7b5c9faca65c6844846f46f9
EricNguyen99 <vanthin7111999@gmail.com>
2021-09-09 17:17:34 +0700

    update icons with emoji-picker-react

--- frontend/src/components/Icons.js
+++ frontend/src/components/Icons.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from "react";
+import Picker, { SKIN_TONE_NEUTRAL } from "emoji-picker-react";
+import { RiUserSmileLine } from "react-icons/ri";
 
-const Icons = ({setContent, content, theme}) => {
-  const reactions = [   
-      '❤️', '😆', '😯', '😢', '😡', '👍', '👎', '😄',
-      '😂', '😍', '😘', '😗', '😚', '😳', '😭', '😓',
-      '😤', '🤤', '👻', '💀', '🤐', '😴', '😷', '😵'
-  ]
+const Icons = ({ setContent, content, theme }) => {
+  //const [chosenEmoji, setChosenEmoji] = useState(null);
+
+  const onEmojiClick = (event, emojiObject) => {
+    setContent(content + emojiObject.emoji);
+  };
 
   return (
-      <div className="nav-item dropdown" 
-      style={{ zIndex: "100", opacity: 1, filter: theme ? 'invert(1)' : 'invert(0)' }}>
-          
-          <span className="nav-link position-relative px-1" id="navbarDropdown" 
-          role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span style={{opacity: 0.4}}>😄</span>
-          </span>
-
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <div className="reactions">
-                  {
-                      reactions.map(icon => (
-                          <span key={icon} onClick={() => setContent(content + icon)}>
-                              {icon}
-                          </span>
-                      ))
-                  }
-              </div>
-          </div>
-              
+    <div className="nav-item dropdown" style={{ zIndex: "100", opacity: 1, filter: theme ? "invert(1)" : "invert(0)" }}>
+      <span className="nav-link position-relative"
+        id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+      >
+        <span style={{ fontSize: "2rem"}}>
+          <RiUserSmileLine />
+        </span>
+      </span>
+      
+      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+        <Picker
+          onEmojiClick={onEmojiClick}
+          disableAutoFocus={true}
+          skinTone={SKIN_TONE_NEUTRAL}
+          groupNames={{ smileys_people: "PEOPLE" }}
+          disableSkinTonePicker={true}
+          />     
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Icons
+export default Icons;
--- frontend/src/components/home/InputComment.js
+++ frontend/src/components/home/InputComment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { createComment } from '../../redux/actions/commentAction'
 import Icons from '../Icons'
+import { BiSend } from "react-icons/bi";
 
 const InputComment = ({ children, post, onReply, setOnReply }) => {
   const [content, setContent] = useState('')
@@ -34,14 +35,14 @@ const InputComment = ({ children, post, onReply, setOnReply }) => {
   return (
     <form className="card-footer comment_input" onSubmit={handleSubmit}>
       {children}
-
+      <span className="pb-2">
+        <Icons setContent={setContent} content={content} theme={theme}/>
+      </span>
       <input type="text" placeholder="Add your comments ..."
         value={content} onChange={e => setContent(e.target.value)} />
       
-      <Icons setContent={setContent} content={content} theme={theme}/>
-
       <button type="submit" className="postBtn">
-        Post
+        <BiSend size="2rem"/>
       </button>
     </form>
   )
